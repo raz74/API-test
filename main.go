927f898be6d42735554bbdd341675d4967d5df28
raaaz <razijam996@gmail.com>
2023-02-19 13:24:59 +0330

    init api

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"api-test/handler"
+	"api-test/repository"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	db := repository.Initialize()
+	h := handler.NewPostgresRepo(db)
+
+	setUpRouts(h)
+}
+
+func setUpRouts(h *handler.PostgresRepo) {
+	e := echo.New()
+	e.POST("/photo", h.UploadFile)
+	e.GET("/photo/:id", h.GetUser)
+	e.Logger.Fatal(e.Start(":3000"))
+}

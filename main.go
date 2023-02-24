69cdd5e0704857cd2899e935cb23aa9b0897359e
raaaz <razijam996@gmail.com>
2023-02-24 22:34:17 +0330

    feat: adding cors middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"api-test/handler"
 	"api-test/repository"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
@@ -15,8 +16,13 @@ func main() {
 
 func setUpRouts(h *handler.PostgresRepo) {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	e.Static("/static", "static")
 	e.POST("/photo", h.UploadFile)
+	e.GET("/photos/", h.GetUsers)
 	e.GET("/photo/:id", h.GetUser)
 	e.Logger.Fatal(e.Start(":3000"))
 }
